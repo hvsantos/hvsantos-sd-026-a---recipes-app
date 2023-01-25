73d684e3ac0a55ec75f1d9f3ab68a03da5ebb659
¨Cassio <cassioschii@gmail.com>
2023-01-25 17:46:08 -0300

    feat: Adicao das rotas

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { Switch, Route } from 'react-router-dom';
+//  import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './pages/Login';
+import Recipes from './pages/Recipes';
+import RecipeInProgress from './pages/RecipeInProgress';
+import DoneRecipes from './pages/DoneRecipes';
+import Profile from './pages/Profile';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route exact path="/meals" component={ Recipes } />
+      <Route exact path="/drinks" component={ Recipes } />
+      <Route
+        exact
+        path="/meals/:id-da-receita/in-progress"
+        component={ RecipeInProgress }
+      />
+      <Route
+        exact
+        path="/drinks/:id-da-receita/in-progress"
+        component={ RecipeInProgress }
+      />
+      <Route exact path="/done-recipes" component={ DoneRecipes } />
+      <Route exact path="/profile" component={ Profile } />
+    </Switch>
   );
 }
 
