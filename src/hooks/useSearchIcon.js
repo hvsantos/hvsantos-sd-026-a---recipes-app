7cf31162b08e5d4a6359934f21a71eab6ba6033b
¨Cassio <cassioschii@gmail.com>
2023-01-26 16:11:10 -0300

    feat: Botão de busca adiciona/remove input de texto

--- src/hooks/useSearchIcon.js
+++ src/hooks/useSearchIcon.js
@@ -1,21 +1,38 @@
+import { useState } from 'react';
 import searchIcon from '../images/searchIcon.svg';
 
 const useSearchIcon = (pageName) => {
+  const [search, setSearch] = useState(false);
+
+  const showRemoveSearch = () => {
+    if (search === true) {
+      setSearch(false);
+    } else {
+      setSearch(true);
+    }
+  };
+
   if (pageName === 'Profile'
     || pageName === 'Done Recipes'
     || pageName === 'Favorite Recipes') {
     return (null);
   }
   return (
-    <object
-      data-testid="search-top-btn"
-      className="search-top-btn"
-      type="image/svg+xml"
-      data={ searchIcon }
-      src={ searchIcon }
-    >
-      search-top-btn
-    </object>
+    <>
+      <button
+        onClick={ showRemoveSearch }
+      >
+        <img
+          data-testid="search-top-btn"
+          className="search-top-btn"
+          type="image/svg+xml"
+          data={ searchIcon }
+          src={ searchIcon }
+          alt="search-icon"
+        />
+      </button>
+      { search === true ? <input type="text" data-testid="search-input" /> : null }
+    </>
   );
 };
 
