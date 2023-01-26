43ab0c13c503479e880308daf39c06577cddef02
¨Cassio <cassioschii@gmail.com>
2023-01-26 09:59:51 -0300

    feat: header com icones adicionados em todas as páginas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import profileTopButton from '../images/profileIcon.svg';
+import useSearchIcon from '../hooks/showSearchButton';
 
-function Header() {
+function Header({ pageName }) {
+  const searchIcon = useSearchIcon(pageName);
   return (
     <div>
-      Header
+      <object
+        data-testid="profile-top-btn"
+        className="profile-top-btn"
+        type="image/svg+xml"
+        data={ profileTopButton }
+        src={ profileTopButton }
+      >
+        profile-top-btn
+      </object>
+      { searchIcon }
+      <h1 data-testid="page-title">
+        { pageName }
+      </h1>
       <SearchBar />
     </div>
   );
