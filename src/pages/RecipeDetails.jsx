ae7cda00a1128519e41dc959ff22f75ebf22f66c
Henrique Vilela <hv_santos@outlook.com>
2023-01-30 19:30:00 -0300

    removed ShareButton - It's a component now

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,17 +1,18 @@
 import React, { useState, useEffect, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
-import clipboardCopy from 'clipboard-copy';
+
 import { DataContext } from '../context/DataContext';
 import MealDetail from '../components/screens/RecipeDetails/MealDetail';
 import DrinksDetails from '../components/screens/RecipeDetails/DrinksDetail';
 import Loading from '../components/Loading';
-import shareIcon from '../images/shareIcon.svg';
+
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
 import useFavorites from '../hooks/UseFavorite';
 import useProgress from '../hooks/useProgress';
 import useDoneRecipe from '../hooks/useDoneRecipe';
+import ShareButton from '../components/ShareButton';
 
 function RecipeDetails(props) {
   const { isFavorite, handleFavorites, hasFavorite } = useFavorites(false);
@@ -19,7 +20,6 @@ function RecipeDetails(props) {
   const { doneRecipe, checkDoneRecipe } = useDoneRecipe();
   const { recipe, setRecipe } = useContext(DataContext);
   const [redirect, setRedirect] = useState(false);
-  const [copy, setCopy] = useState(false);
   const { match: { params: { id }, url } } = props;
   const where = url.split('/')[1];
 
@@ -33,15 +33,6 @@ function RecipeDetails(props) {
       .then((response) => setRecipe(response));
   }, [url, id, where, setRecipe]);
 
-  function handleShare() {
-    clipboardCopy(`http://localhost:3000${url}`);
-    setCopy(true);
-    // const timeAlert = 2000;
-    // setTimeout(() => {
-    //   setCopy(false);
-    // }, timeAlert);
-  }
-
   useEffect(() => {
     if (recipe) {
       const savedRecipe = recipe[where][0];
@@ -54,8 +45,6 @@ function RecipeDetails(props) {
   if (!recipe) {
     return <Loading />;
   }
-
-  console.log(inProgress);
 
   if (redirect) {
     return <Redirect to={ `${url}/in-progress` } />;
@@ -71,24 +60,16 @@ function RecipeDetails(props) {
           left: '80%',
         } }
       >
-        <button
-          type="button"
-          data-testid="share-btn"
-          onClick={ handleShare }
-        >
-          <img
-            src={ shareIcon }
-            alt="share-button"
+        <ShareButton url={ url } testId="share-btn" />
+        <div>
+          <input
+            type="image"
+            data-testid="favorite-btn"
+            onClick={ () => handleFavorites(recipe[where][0], where) }
+            src={ isFavorite ? blackHeartIcon : whiteHeartIcon }
+            alt="favorite-button"
           />
-        </button>
-        { copy ? <p>Link copied!</p> : '' }
-        <input
-          type="image"
-          data-testid="favorite-btn"
-          onClick={ () => handleFavorites(recipe[where][0], where) }
-          src={ isFavorite ? blackHeartIcon : whiteHeartIcon }
-          alt="favorite-button"
-        />
+        </div>
       </div>
       { where === 'meals'
         ? <MealDetail meal={ recipe.meals } />
