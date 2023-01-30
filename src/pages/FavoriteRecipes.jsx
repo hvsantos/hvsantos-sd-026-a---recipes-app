320a5710a214e78698be888cc788538847a6b7fc
TulioTofani <tuliotoff@gmail.com>
2023-01-30 18:41:13 -0300

    adiciona requisito 55 e 56

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -5,24 +5,24 @@ import usePageName from '../hooks/usePageName';
 import { saveItem, getItem } from '../components/localStorage';
 import shareIcon from '../images/shareIcon.svg';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
-// import useFavorites from '../hooks/UseFavorite';
+import { Link } from 'react-router-dom';
 
 function FavoriteRecipes() {
-  // const { isFavorite, handleFavorites, hasFavorite } = useFavorites(false);
   const pageName = usePageName();
   const [favorites, setFavorites] = useState([]);
   const [hasFavorite, setHasFavorite] = useState(true);
   const [copy, setCopy] = useState(false);
+  const [showFavorites, setShowFavorites] = useState([]);
 
   useEffect(() => {
     const storageFavorites = getItem('favoriteRecipes');
     const validStorage = storageFavorites || [];
     setFavorites(validStorage);
     setHasFavorite(!!validStorage);
+    setShowFavorites(validStorage);
   }, []);
 
   function handleShare(type, id) {
-    console.log(id, type);
     const where = type === 'meal' ? 'meals' : 'drinks';
     clipboardCopy(`http://localhost:3000/${where}/${id}`);
     setCopy(true);
@@ -30,13 +30,24 @@ function FavoriteRecipes() {
 
   function handleNotFavorite(id) {
     const control = favorites;
-    const filteredFavorite = control.filter((el) => el.id !== id);
-    saveItem('favoriteRecipes', filteredFavorite);
-    setFavorites(filteredFavorite);
+    const control2 = showFavorites;
+    const removedFavoriteFilter = control2.filter((el) => el.id !== id);
+    const removedFavorite = control.filter((el) => el.id !== id);
+    saveItem('favoriteRecipes', removedFavorite);
+    setFavorites(removedFavorite);
+    setShowFavorites(removedFavoriteFilter);
   }
 
-  console.log(favorites);
-  console.log(hasFavorite);
+  function handleFilter({ target }) {
+    const control = favorites;
+    const filteredFavorite = control.filter((el) => el.type === target.value);
+    setShowFavorites(filteredFavorite);
+  }
+
+  function removeFilter() {
+    const control = favorites;
+    setShowFavorites(control);
+  }
 
   return (
     <div>
@@ -46,36 +57,44 @@ function FavoriteRecipes() {
         <button
           type="button"
           data-testid="filter-by-all-btn"
+          value="meal"
+          onClick={ removeFilter }
         >
           All
         </button>
         <button
           type="button"
           data-testid="filter-by-meal-btn"
+          value="meal"
+          onClick={ handleFilter }
         >
           Meals
         </button>
         <button
           type="button"
           data-testid="filter-by-drink-btn"
+          value="drink"
+          onClick={ handleFilter }
         >
           Drinks
         </button>
       </div>
       { hasFavorite ? (
-        favorites.map((el, index) => (
+        showFavorites.map((el, index) => (
           <div key={ el.id }>
             <p data-testid={ `${index}-horizontal-top-text` }>
               { el.type === 'meal' ? `${el.nationality} - ${el.category}`
                 : el.alcoholicOrNot }
             </p>
-            <p data-testid={ `${index}-horizontal-name` }>{ el.name }</p>
-            <img
-              src={ el.image }
-              alt={ el.name }
-              data-testid={ `${index}-horizontal-image` }
-              width="200"
-            />
+            <Link to={ el.type === 'meal' ? `/meals/${el.id}` : `/drinks/${el.id}` }>
+              <p data-testid={ `${index}-horizontal-name` }>{ el.name }</p>
+              <img
+                src={ el.image }
+                alt={ el.name }
+                data-testid={ `${index}-horizontal-image` }
+                width="200"
+              />
+            </Link>
             <input
               type="image"
               data-testid={ `${index}-horizontal-share-btn` }
