d2f85c835ce0dce80e463d48e211230c083c5268
¨Cassio <cassioschii@gmail.com>
2023-01-30 15:55:08 -0300

    feat: Logica de search implementada

--- src/components/Search/SearchInput.jsx
+++ src/components/Search/SearchInput.jsx
@@ -1,24 +1,37 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 function SearchInput() {
   const [search, setSearch] = useState('');
 
+  const history = useHistory();
+  const path = history.location.pathname;
+
+  const mealOrDrinkApi = () => {
+    if (path === '/meals') {
+      return 'themealdb';
+    } if (path === '/drinks') {
+      return 'thecocktaildb';
+    }
+  };
+
   const handleClickButton = async () => {
     const inputValue = document.querySelector('[data-testid="search-input"]').value;
     if (search === 'ingredient') {
-      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${inputValue}`);
+      const response = await fetch(`https://www.${mealOrDrinkApi()}.com/api/json/v1/1/filter.php?i=${inputValue}`);
       const data = await response.json();
+      console.log(data);
       return data;
     } if (search === 'name') {
-      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`);
+      const response = await fetch(`https://www.${mealOrDrinkApi()}.com/api/json/v1/1/search.php?s=${inputValue}`);
       const data = await response.json();
       return data;
     } if (search === 'first-letter') {
       if (inputValue.length > 1) {
-        alert('Your search must have only 1 (one) character');
+        global.alert('Your search must have only 1 (one) character');
         return;
       }
-      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${inputValue}`);
+      const response = await fetch(`https://www.${mealOrDrinkApi()}.com/api/json/v1/1/search.php?f=${inputValue}`);
       const data = await response.json();
       return data;
     }
