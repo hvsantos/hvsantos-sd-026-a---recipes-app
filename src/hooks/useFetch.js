fcb9a3cf82c510ccef2cefdac0c605d51ff0b38b
TulioTofani <tuliotoff@gmail.com>
2023-02-01 18:57:14 -0300

    adiciona profile sem testes

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,12 +1,12 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
 function useFetch() {
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
   // const [error, setError] = useState(null);
 
   const getFetch = async (url) => {
     try {
-      setIsLoading(true);
+      // setIsLoading(true);
 
       const response = await fetch(url);
       const apiResponse = await response.json();
@@ -15,10 +15,10 @@ function useFetch() {
     } catch (e) {
       // setError(e);
     } finally {
-      setIsLoading(false);
+      // setIsLoading(false);
     }
   };
-  return { isLoading, getFetch };
+  return { getFetch };
 }
 
 export default useFetch;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import usePageName from '../hooks/usePageName';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { getItem } from '../components/localStorage';
 
 function Profile() {
   const pageName = usePageName();
+  const [user, setUserEmail] = useState({ email: '' });
+  const history = useHistory();
+
+  useEffect(() => {
+    const validUser = getItem('user') ? getItem('user') : '';
+    setUserEmail(validUser);
+  }, []);
+
+  const redirectFavorites = () => {
+    history.push('/favorite-recipes');
+  };
+
+  const redirectDone = () => {
+    history.push('/done-recipes');
+  };
+
+  const handleLogout = () => {
+    localStorage.clear('user');
+    history.push('/');
+  };
+
   return (
     <div>
       <Header pageName={ pageName } />
-      Profile
+      <div>
+        <p data-testid="profile-email">{ user.email }</p>
+      </div>
+      <div>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ redirectDone }
+        >
+          Done Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ redirectFavorites }
+        >
+          Favorite Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ handleLogout }
+        >
+          Logout
+        </button>
+      </div>
       <Footer />
     </div>
   );
